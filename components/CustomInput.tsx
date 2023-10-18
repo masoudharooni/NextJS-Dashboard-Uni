be6f8f10f46f346590d0fc53904fd18e9a4648fb
masoudharooni <masoudharooni50@gmail.com>
2023-10-18 19:46:07 +0330

    [RTL - DONE] TextField MUI component

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { ThemeProvider, createTheme } from "@mui/material";
 import TextField from "@mui/material/TextField";
+import { useLayoutEffect } from "react";
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import { prefixer } from "stylis";
 
 type TProps = {
   label: string;
@@ -17,14 +23,28 @@ export default function CustomInput({
   variant = "outlined",
   placeholder,
 }: TProps) {
+  useLayoutEffect(() => {
+    document.body.setAttribute("dir", "rtl");
+  }, []);
+  const rtlTheme = createTheme({
+    direction: "rtl",
+  });
+  const cacheRtl = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
   return (
-    <TextField
-      label={label}
-      variant={variant}
-      fullWidth
-      value={value}
-      placeholder={placeholder}
-      onChange={(e) => onChange(e.target.value)}
-    />
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={rtlTheme}>
+        <TextField
+          label={label}
+          variant={variant}
+          fullWidth
+          value={value}
+          placeholder={placeholder}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
